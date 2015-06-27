b8e06d5cdbc3f8f8d5f2eb4d421ec705982b65ce
JONNALAGADDA Srinivas <js@ojuslabs.com>
2015-06-27 11:06:31 +0530

    Add a `Word` type of token

--- tokenizer/word.go
+++ tokenizer/word.go
@@ -1,15 +1,42 @@
 package tokenizer
 
-// WordProps qualifies a token whose type is one of `TokMayBeWord` or
-// `TokWord`.
+// Word represents a token whose type is one of `TokMayBeWord` or
+// `TokWord`, and qualifies it.
 //
 // It holds information regarding the so-called IOB (Inside, Outside,
 // Beginning) status of the token, its lemma form (in case of a word),
 // its part of speech (in case of a word), etc.
-type WordProps struct {
-	Word   Token  // The token being qualified.
-	IsWord bool   // TokMayBeWord, etc. (false) vs. TokWord (true)
-	IOB    byte   // 'B', 'I' or 'O'
-	Lemma  string // Lemma form
-	PoS    string // Part of Speech
+type Word struct {
+	token TextToken // Actual text and its properties
+	iob   byte      // 'B', 'I' or 'O'
+	lemma string    // Lemma form
+	pos   string    // Part of Speech
+}
+
+func (w *Word) Text() string {
+	return w.token.text
+}
+
+func (w *Word) Begin() int {
+	return w.token.begin
+}
+
+func (w *Word) End() int {
+	return w.token.end
+}
+
+func (w *Word) Type() TokenType {
+	return w.token.ttype
+}
+
+func (w *Word) IOB() byte {
+	return w.iob
+}
+
+func (w *Word) Lemma() string {
+	return w.lemma
+}
+
+func (w *Word) POS() string {
+	return w.pos
 }
